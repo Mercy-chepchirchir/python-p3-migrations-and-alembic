c2539a5150a5dd7b8e72e7f78bab3b7c455ffa3e
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-09-06 14:37:05 +0300

    done

---
+++ app/migrations/versions/174dc91a23aa_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 174dc91a23aa
+Revises: 
+Create Date: 2023-09-05 00:21:10.018277
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '174dc91a23aa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ app/migrations/versions/c24387147d77_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: c24387147d77
+Revises: 174dc91a23aa
+Create Date: 2023-09-05 09:07:01.061984
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c24387147d77'
+down_revision = '174dc91a23aa'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
